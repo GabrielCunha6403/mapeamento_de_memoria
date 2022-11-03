a4265527ae45c616db093fd7f565676adc4ac97f
Gabriel Cunha <gabrielcunha2012@gmail.com>
2022-11-03 18:32:31 -0300

    subindo trabalho

--- mapeamentoDeMemoria.js
+++ mapeamentoDeMemoria.js
@@ -1,13 +1,10 @@
-var tamanho_memoria_principal = parseInt(prompt("Tamanho da memória Principal (Bytes):"));
-var tamanho_memoria_cache = parseInt(prompt("Tamanho da memória Cache (Bytes):"));
-var tamanho_bloco = parseInt(prompt("Tamanho do Bloco (Bytes):"));
+var tamanho_memoria_principal = parseInt(prompt("Tamanho da Memória Principal (Bytes):"));
+var tamanho_memoria_cache = parseInt(prompt("Tamanho da Memória Cache (Bytes):"));
+var tamanho_bloco = parseInt(prompt("Tamanho do Bloco/Linha (Bytes):"));
+var read_address = prompt("Digite o endereço a ser lido:");
 
-var tamanho_linha = tamanho_memoria_cache / tamanho_bloco;
-
-var largura_bloco_linha = Math.log2(tamanho_memoria_cache);
+var tamanho_enderenco = Math.log2(tamanho_memoria_principal);
 var qtd_linhas;
-
-var tamanho_enderenco = Math.log2(tamanho_memoria_principal * 8);
 
 var memoria_principal = new Array(tamanho_memoria_principal);
 var memoria_cache = new Array(tamanho_memoria_cache);
